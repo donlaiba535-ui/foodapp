f05790979fab351a8ec51a7c6913a2c9e049eb43
laiba713 <donlaiba535@gmail.com>
2025-10-12 00:21:03 +0500

    too

--- hello.js
+++ hello.js
@@ -16,3 +16,19 @@ let display = document.getElementById('display');
       display.value = '';
     }
     
+    const para =document.querySelector("p");
+    const input=document.querySelector("input");
+    const btn=document.querySelector("button");
+    btn.addEventListener("click",() => {
+    para.textContent = "output :";
+    const num = input.value;
+    input.value ="";
+    input.focus ();
+for (let i=1; i++; i<=num){
+    let sqroot= Math.sqrt(i);
+    if (Math.floor(sqrt) !== sqrt){
+        continue;
+    }
+    para.textContent+= `${i}`;
+}
+    });
